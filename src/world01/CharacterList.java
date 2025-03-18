b4d727597702a945d1f874cb4f4356acc18a5ffb
myoshito <marubomu@gmail.com>
2025-03-18 19:14:55 +0900

    20250318

---
+++ src/world01/CharacterList.java
@@ -0,0 +1,59 @@
+//package world01;
+//
+//public class CharacterList {
+//	class Ari extends HuanCharacter {
+//	    public Ari() {
+//	        super("Ari", "Red-brown long hair", "Reddish skin", "-", "Light orange shirt, teal pants, gray shoes", "alex.png");
+//	    }
+//	}
+//
+//	class Alex extends HuanCharacter {
+//	    public Alex() {
+//	        super("Alex", "Orange-gold long hair", "Light orange skin", "Dark green", "Light green shirt, brown pants, gray boots", "alex.png");
+//	    }
+//	}
+//
+//	class Efe extends HuanCharacter {
+//	    public Efe() {
+//	        super("Efe", "Purple short hair", "Brown skin", "Red-purple", "Light blue cardigan over black blouse, dark purple pants", "efe.png");
+//	    }
+//	}
+//
+//	class Kai extends HuanCharacter {
+//	    public Kai() {
+//	        super("Kai", "Blonde long hair", "Light brown skin", "Green", "Purple haori, black innerwear, red sandals", "kai.png");
+//	    }
+//	}
+//
+//	class Sunny extends HuanCharacter {
+//	    public Sunny() {
+//	        super("Sunny", "Black short hair", "Orange skin", "Black", "Green shirt, blue overalls, white-gray shoes", "sunny.png");
+//	    }
+//	}
+//
+//	class Steve extends HuanCharacter {
+//	    public Steve() {
+//	        super("Steve", "Dark brown short hair", "Tanned skin", "Bluish purple", "Blue T-shirt, blue pants, gray shoes", "steve.png");
+//	    }
+//	}
+//
+//	class Zuri extends HuanCharacter {
+//	    public Zuri() {
+//	        super("Zuri", "Black curly short hair", "Dark brown skin", "Dark brown", "Red polo shirt, beige pants, black shoes", "zuri.png");
+//	    }
+//	}
+//
+//	class Noor extends HuanCharacter {
+//	    public Noor() {
+//	        super("Noor", "Dark brown dreadlocks", "Reddish brown skin", "Brown", "Red jacket, black shirt, loose green pants", "noor.png");
+//	    }
+//	}
+//
+//	class Makena extends HuanCharacter {
+//	    public Makena() {
+//	        super("Makena", "Purplish dark brown hair", "Dark brown skin", "Dark brown", "Yellow sweater, light red pants, black-white shoes", "makena.png");
+//	    }
+//	}
+//
+//
+//}
